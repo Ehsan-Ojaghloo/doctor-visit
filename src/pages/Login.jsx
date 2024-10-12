152de66131099076e60011a262972a03df259e00
parhampi <pahampi9@gmail.com>
2024-10-12 20:23:11 +0330

    making a custom hook for changing pages title

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,22 +4,23 @@ import 'react-toastify/dist/ReactToastify.css';
 import { json, useNavigate } from 'react-router-dom'
 import "./Register.scss"
 import axios from 'axios'
+import { useTitle } from '../hooks/useTitle';
 
 function Register() {
 
     const [users, setUsers] = useState([]);
+
     const getUsername = useRef()
     const getPassword = useRef()
 
     const navigate = useNavigate()
 
-    // useEffect(() => {
-
-    // }, [])
+    useTitle("Login Page")
 
     const loginUserData = async () => {
         const response = await axios.get(`http://localhost:3000/users`)
         console.log(response.data)
+
 
         const matchUser = users.find(
             data => data.username === getUsername && data.password === getPassword
@@ -31,19 +32,18 @@ function Register() {
             console.log('No matching user found');
         }
     }
-}
 
-return (
-    <div className='register-con'>
-        <ToastContainer />
-        <div className="register">
-            <h1> Login </h1>
-            <input type="text" ref={getUsername} placeholder='Username' />
-            <input type="password" ref={getPassword} placeholder='Password' />
-            <button onClick={regUserData}> Login </button>
+    return (
+        <div className='register-con'>
+            <ToastContainer />
+            <div className="register">
+                <h1> Login </h1>
+                <input type="text" ref={getUsername} placeholder='Username' />
+                <input type="password" ref={getPassword} placeholder='Password' />
+                <button onClick={loginUserData}> Login </button>
+            </div>
         </div>
-    </div>
-)
+    )
 }
 
 export default Register
